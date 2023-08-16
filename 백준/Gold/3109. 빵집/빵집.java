60978dfd0d27ad056f429085638ba90117f52455
cwj980119 <42488191+cwj980119@users.noreply.github.com>
2023-08-16 14:26:04 +0900

    [Gold II] Title: 빵집, Time: 528 ms, Memory: 113252 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static char[][] map;
+	static boolean[][] check;
+	static int r, c;
+	static int t;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		map = new char[r+2][c+2];
+		check = new boolean[r+2][c+2];
+		String str;
+		for(int i = 1; i <= r; i++) {
+			str = br.readLine();
+			for(int j = 1; j <=c ;j++) {
+				map[i][j] = str.charAt(j-1);
+			}
+		}
+		int cnt = 0;
+		for(int i = 1; i <= r;i++) {
+			if(find(i,1)) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+	
+	static boolean find(int cr, int cc) {
+//		for(int i = 1; i<=r;i++) {
+//			for(int j = 1; j <= c; j++) {
+//				System.out.print(map[i][j]);
+//			}
+//			System.out.println();
+//		}
+//		System.out.println("========="+cr + " " + cc+"===========");
+		int nr, nc;
+		int[] dir = {-1, 0, 1};
+		if(cc == c) return true;
+		for(int i = 0; i < 3; i++) {
+			nr = cr + dir[i];
+			nc = cc + 1;
+			if(map[nr][nc] == '.' && !check[nr][nc]) {
+				map[nr][nc] = '-';
+				if(find(nr, nc)) return true;
+				map[nr][nc] = '.';
+			}
+			else check[nr][nc] = true;
+		}
+		check[cr][cc] = true;
+		return false;
+	}
+
+}
