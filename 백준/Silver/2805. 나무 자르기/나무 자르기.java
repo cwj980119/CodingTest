f6a3c5a278ce6325c99b0c1d86fc97f932dcc9b5
cwj980119 <42488191+cwj980119@users.noreply.github.com>
2023-08-16 15:59:09 +0900

    [Silver II] Title: 나무 자르기, Time: 2184 ms, Memory: 134992 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		Integer[] arr = new Integer[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr, Collections.reverseOrder());
+		
+		long max = arr[0];
+		long cnt = 0;
+		for(int i = 1; i<n;i++) {
+			cnt += (max - arr[i]) * i;
+			max = arr[i];
+			if(cnt > m) {
+				max += (cnt - m)/i;
+				break;
+			}
+		}
+		if(cnt < m) {
+			long rest = (long) m - cnt;
+			max -= Math.ceil((float)rest/n);
+		}
+		System.out.println(max);
+		
+	}
+
+}
