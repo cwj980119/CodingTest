88f24109ccffc7d78ea20bc906eb8f3f2cc5a062
cwj980119 <42488191+cwj980119@users.noreply.github.com>
2023-08-21 13:42:25 +0900

    [Bronze II] Title: 분산처리, Time: 80 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/1009. 분산처리/분산처리.py
@@ -0,0 +1,7 @@
+can = [[10], [1], [2, 4, 8, 6], [3, 9, 7, 1], [4, 6], [5], [6], [7, 9, 3, 1], [8, 4, 2, 6], [9, 1]]
+
+T = int(input())
+
+for _ in range(T):
+    a, b = map(int, input().split())
+    print(can[a%10][(b - 1) % len(can[a%10])])
